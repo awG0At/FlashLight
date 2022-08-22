d19d699196362647e219da35656c8c2e0c359dc4
aw <89423@psdschools.org>
2022-08-22 09:51:59 -0600

    Initial Commit

---
+++ FlashLight/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FlashLight
+//
+//  Created by Williams, Alexander - Student on 8/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onButtonTap(_ sender: UIButton) {self.view.backgroundColor = UIColor.blue
+    }
+    
+
+}
+
